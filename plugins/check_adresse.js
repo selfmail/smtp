e2a7db2e4eec6301492b0907b631434142993633
henri <henri.me@skiff.com>
2024-10-17 21:41:52 +0200

    added check_adresse plugin

---
+++ plugins/check_adresse.js
@@ -0,0 +1,19 @@
+exports.register = function () {
+    this.loginfo("Plugin check_email_in_db loaded");
+    this.register_hook('rcpt', 'check_email_in_db');
+};
+
+// check if the recipient is in the db
+exports.check_email_in_db = function (next, connection, params) {
+    const email = params[0].address(); 
+
+    this.loginfo(email, "This is the email")
+        if (email == "henri") {
+            this.loginfo(`E-Mail-Adresse ${email} nicht in der Datenbank gefunden.`);
+            return next(DENY, "Empfänger nicht autorisiert.");
+        } else if (email == "henri@selfmail.app") {
+            this.loginfo(`E-Mail-Adresse ${email} nicht in der Datenbank gefunden.`);
+            return next(DENY, "Empfänger nicht autorisiert.");
+        }
+        return next()
+};
