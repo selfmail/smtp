de1d79d61874967f2e6354f6a393c5e6f0efab47
henri <henri.me@skiff.com>
2024-10-22 10:34:04 +0200

    improveed the compiling script

--- scripts/compile.ts
+++ scripts/compile.ts
@@ -1,70 +1,90 @@
 /* 
-This is a script for compiling the plugins.
-We need to remove the first line (the haraka import)
-because otherwise, it would trigger an error during
-production. We are searching for directorys within
-the src folder. Every plugin.ts file in such a directory
-has to have an special export, where the name and the
-hook is exported. In this file, we are compiling this special
-plugin.ts file into an .js file in the `./plugin`-directory for 
-haraka. Every normal file will be exported in a `./helper` directory,
-also as an .js file. We are also editing the imports, to match the 
-new folder structure, and we are removing every haraka import,
-if not an error will be triggered from haraka.
+	This is a script for compiling the plugins.
+	We need to remove the first line (the haraka import)
+	because otherwise, it would trigger an error during
+	production. We are searching for directorys within
+	the /src/ folder. Every plugin.ts file in such a directory
+	has to have an special export, where the name and the
+	hook is exported. In this file, we are compiling this special
+	plugin.ts file into an .js file in the `./plugin`-directory for 
+	haraka. Every normal file will be exported in a `./helper` directory,
+	also as an .js file. We are also editing the imports, to match the 
+	new folder structure, and we are removing every haraka import,
+	if not an error will be triggered from haraka.
 */
 
-import { writeFile, readdir, readFile } from "node:fs/promises";
-import { statSync } from "node:fs";
-import path from "node:path";
+import { ConsolaInstance } from "consola";
 import { transform } from "esbuild";
+import { statSync } from "node:fs";
+import { readdir, readFile } from "node:fs/promises";
+import path from "node:path";
 
+
+// object of the default export from a plugin.ts file
 type Plugin = {
+	// name of this plugin
 	name: string;
+	// code which is inside of this plugin
+	code: string;
 };
+
+// compile ts to js, used for compiling the plugins from the /src/ folder to js into the plugins folder
+async function compileTypeScript(code: string) {
+	let resultCode: string;
+	const result = await transform(code, {
+		loader: "ts",
+		format: "cjs",
+	})
+	return result.code;
+}
+
+// get the content of every plugin.ts file inside the /src/ folder
+async function getAllPluginFiles({
+	dirPath = "./src",
+	pluginArray = [],
+	consola
+}: {
+	dirPath?: string,
+	pluginArray?: string[],
+	consola: ConsolaInstance
+}) {
+	const files = await readdir(dirPath);
+	consola.log(files)
+	for (const file of files) {
+		const filePath = path.join(dirPath, file);
+
+		if (statSync(filePath).isDirectory()) {
+			// every plugin.ts file is a plugin
+			const subfolder = await readdir(filePath);
+			for await (const subfolderFile of subfolder) {
+				// check if the file is an plugin, if not, go to the next file
+				if (subfolderFile === "plugin.ts") {
+					const pluginFile = await readFile(`./${filePath}/plugin.ts`);
+					consola.log(`Read file plugin.ts in ${filePath}`)
+					// modify this plugin file
+					consola.success("Successfully modified the ");
+				}
+			}
+		} else {
+		}
+	}
+	return pluginArray;
+}
+// compile a plugin.ts file into a js file, for the haraka plugin system
+async function compilePlugin() {
+
+}
+
+async function savePlugins() {
+
+}
 
 // function to use consola and the other async functions
 (async () => {
-	const consola = await (await import("consola")).default;
-	consola.log("This is a test message");
+	const consola = (await import("consola")).default;
+	consola.info("Starting the compiling process.")
+	const plugins = await getAllPluginFiles({
+		consola
+	})
 
-	async function compileTypeScript(code: string) {
-		let resultCode: string;
-		const result = await transform(code, {
-			loader: "ts", 
-			format: "cjs",
-		})
-		return result.code;
-	}
-
-	async function getAllPluginFiles(
-		dirPath = "./src",
-		pluginArray: string[] = [],
-	) {
-		const files = await readdir(dirPath);
-		consola.log(files)
-
-		for await (const file of files) {
-			const filePath = path.join(dirPath, file);
-
-			if (statSync(filePath).isDirectory()) {
-				// every plugin.ts file is a plugin
-				const subfolder = await readdir(filePath);
-				for await (const subfolderFile of subfolder) {
-					// check if the file is an plugin, if not, go to the next file
-					if (subfolderFile === "plugin.ts") {
-						const pluginFile = await readFile(`./${filePath}/plugin.ts`);
-						consola.log(`Read file plugin.ts in ${filePath}`)
-						// modify this plugin file
-						consola.success("Successfully modified the ");
-					}
-				}
-			} else {
-			}
-		}
-		return pluginArray;
-	}
-	await getAllPluginFiles()
-	async function writePlugins() {
-		const files = await readdir("../src");
-	}
 })();
